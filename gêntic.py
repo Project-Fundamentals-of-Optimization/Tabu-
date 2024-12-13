1ea5ca0d3309a1b464594a9bfa1307e061c8d4dd
Bangchis <tienqldtdata@gmail.com>
2024-12-13 16:42:08 +0700

    commit dau tien

---
+++ gêntic.py
@@ -0,0 +1,367 @@
+# Đường dẫn tới file
+import random
+from queue import PriorityQueue
+import sys
+
+import time
+
+# Bắt đầu đo thời gian
+start_time = time.time()
+
+
+file_path = "/mnt/c/Users/Admin/Desktop/code python/data.txt"
+
+# Đọc file và xử lý dữ liệu
+with open(file_path, "r") as f:
+    # Đọc dòng 1: chứa N và K
+    first_line = f.readline().strip()
+    n, k = map(int, first_line.split())
+
+    # Đọc dòng 2: chứa d(1), d(2), ..., d(N)
+    second_line = f.readline().strip()
+    d = [0] + list(map(int, second_line.split()))
+
+    # Đọc các dòng tiếp theo: ma trận t
+    distance_matrix = []
+    for _ in range(n+1):
+        row = list(map(int, f.readline().strip().split()))
+        distance_matrix.append(row)
+
+"""# khởi tạo trạng thái ban đầu"""
+
+
+#########################################
+
+
+def generate_random_sequence4(n, k):
+    k = k - 1
+
+    # Ensure every number from 0 to k exists at least once
+    sequence = 4 * list(range(k + 1))
+
+    # Fill the remaining elements with random numbers from 0 to k
+    sequence += [random.randint(0, k) for _ in range(n - 4 * (k + 1))]
+
+    # Shuffle the sequence to randomize order
+    random.shuffle(sequence)
+
+    return sequence
+
+
+def generate_random_sequence6(n, k):
+    k = k - 1
+    # Ensure every number from 0 to k exists at least once
+    sequence = 6 * list(range(k + 1))
+
+    # Fill the remaining elements with random numbers from 0 to k
+    sequence += [random.randint(0, k) for _ in range(n - 6 * (k + 1))]
+
+    # Shuffle the sequence to randomize order
+    random.shuffle(sequence)
+
+    return sequence
+
+
+def generate_random_sequence1(n, k):
+    k = k - 1
+    # Ensure every number from 0 to k exists at least once
+    sequence = 1 * list(range(k + 1))
+
+    # Fill the remaining elements with random numbers from 0 to k
+    sequence += [random.randint(0, k) for _ in range(n - 1 * (k + 1))]
+
+    # Shuffle the sequence to randomize order
+    random.shuffle(sequence)
+
+    return sequence
+
+
+def generate_random_sequence_all(n, k):
+    base_count = n // k
+    sequence = [i for i in range(k) for _ in range(base_count)]
+
+    # Số phần tử còn lại cần bổ sung
+    remaining = n - len(sequence)
+
+    # Thêm các số ngẫu nhiên từ 0 đến k-1
+    random_part = [random.randint(0, k-1) for _ in range(remaining)]
+    sequence.extend(random_part)
+
+    # Trộn lại dãy để đảm bảo tính ngẫu nhiên
+    random.shuffle(sequence)
+    return sequence
+
+
+def gen_state(n):
+    prop = random.random()
+    if prop < 0.03:
+        state = generate_random_sequence_all(n, k)
+    elif prop < 0.4:
+        state = generate_random_sequence6(n, k)
+    elif prop < 0.7:
+        state = generate_random_sequence4(n, k)
+    else:
+        state = generate_random_sequence1(n, k)
+    return state
+
+
+initial_state = gen_state(n)
+
+###########################################################################
+"""## hàm fitness"""
+
+
+def greedy_search(worker_k, store_path):
+    route_of_k = store_path[worker_k]
+    start_node = 0
+    set_of_nodes = set(route_of_k)
+    optimize_path = []
+    while set_of_nodes:
+        temp = []
+        for i in set_of_nodes:
+            temp.append((distance_matrix[start_node][i], i))
+        start_node = min(temp)[1]
+        optimize_path.append(start_node)
+        set_of_nodes.remove(start_node)
+        continue
+
+    return optimize_path
+
+
+def fitness(state, K):
+    store_path = {}
+    for i, k in enumerate(state):
+        store_path[k] = store_path.get(k, []) + [i + 1]
+    for k in range(K):
+        store_path[k] = greedy_search(k, store_path)
+
+    total_distance_of_each_k = [0 for _ in range(K)]
+
+    for worker in range(K):
+        pre = 0
+        for node in store_path[worker] + [0]:  # [2,4,5,0]
+
+            total_distance_of_each_k[worker] += distance_matrix[pre][node] + d[node]
+
+            pre = node
+
+    return max(total_distance_of_each_k)
+
+# print(initial_state)
+# print(fitness(initial_state,k))
+
+
+"""# khởi tạo các giá trị ban đầu vào population"""
+
+population = PriorityQueue()
+MAX_SIZE = 20
+top_g = [None, None]  # g<10 push ~ O(1) but g -> 100 slow
+is_added = {}
+
+
+def queue_push(v, top_g, is_added, population):
+
+    if is_added.get(tuple(v[1]), None) != None:
+        return None
+    elif top_g[0] is None:
+        top_g[0] = v
+        is_added[tuple(top_g[0][1])] = 1
+        population.put(v)
+    elif top_g[1] is None:
+        if -v[0] < -top_g[0][0]:
+            top_g[1] = top_g[0]
+            top_g[0] = v
+            is_added[tuple(top_g[0][1])] = 1
+            population.put(v)
+        else:
+            top_g[1] = v
+            is_added[tuple(top_g[1][1])] = 1
+            population.put(v)
+    else:
+        if -v[0] < -top_g[0][0]:
+            top_g[1] = top_g[0]
+            top_g[0] = v
+            is_added[tuple(top_g[0][1])] = 1
+            population.put(v)
+        elif -v[0] < -top_g[1][0]:
+            top_g[1] = v
+            is_added[tuple(top_g[1][1])] = 1
+            population.put(v)
+        else:
+            is_added[tuple(v[1])] = 1
+            population.put(v)
+
+    if population.qsize() > MAX_SIZE:
+        return population.get()
+        print("pop")  # get = pop
+    return None
+
+
+for times in range(12000):
+    temp_state = gen_state(n)
+
+    v = (-fitness(temp_state, k), temp_state)
+    queue_push(v, top_g, is_added, population)
+
+
+temp_list = []
+
+while not population.empty():
+    item = population.get()
+    temp_list.append(item)
+ # In từng phần tử
+
+# Đưa lại các phần tử vào PriorityQueue
+for item in temp_list:
+    population.put(item)
+
+################################################
+
+
+PROP_LOVE = 0.03
+
+
+def check_legit_childe(state, K):
+    for i in range(K):
+        if i not in state:
+            return False
+    return True
+
+
+def take_out_take_back():
+    temp_list = []
+
+    while not population.empty():
+
+        item = population.get()
+
+        temp_list.append(item)
+    # Đưa lại các phần tử vào PriorityQueue
+    for item in temp_list:
+        population.put(item)
+
+    return temp_list
+#############################################
+
+
+def make_love(top_g, K, PROP_LOVE):
+    prop = random.random()
+    new_state = []
+    # unpack top_g -> parents
+    p1, p2 = top_g
+    _, p1 = p1
+    _, p2 = p2
+    # make_love
+    mutation = gen_state(n)
+    if prop < 0.2:
+
+        for i in range(len(p1)):
+            if random.random() < PROP_LOVE:
+                new_state.append([p1[i], mutation[i]][random.randint(0, 1)])
+            else:
+                new_state.append([p1[i], p2[i]][random.randint(0, 1)])
+    elif prop < 0.4:
+
+        index = random.randint(20, len(p1)-20)
+        new_state = p1[:index] + p2[index:]
+
+    elif prop < 0.7:
+        # thay đổi 1 phần
+        for i in range(len(p1)):
+            if random.random() < 0.6:
+                new_state.append(p1[i])
+                continue
+
+            if random.random() < PROP_LOVE:
+                new_state.append([p1[i], mutation[i]][random.randint(0, 1)])
+            else:
+                new_state.append([p1[i], p2[i]][random.randint(0, 1)])
+    elif prop < 0.9:
+        for i in range(len(p1)):
+            if random.random() < 0.6:
+                new_state.append(p2[i])
+                continue
+
+            if random.random() < PROP_LOVE:
+                new_state.append([p1[i], mutation[i]][random.randint(0, 1)])
+            else:
+                new_state.append([p1[i], p2[i]][random.randint(0, 1)])
+    elif prop < 0.95:
+        for i in range(len(p1)):
+            if i % 2 == 0:
+                new_state.append(p2[i])
+                continue
+            else:
+                new_state.append(p1[i])
+                continue
+    else:
+        for i in range(len(p1)):
+            if i % 2 == 0:
+                new_state.append(p1[i])
+                continue
+            else:
+                new_state.append(p2[i])
+                continue
+
+    # avoid 1 worker carry all
+    if check_legit_childe(new_state, K):
+        return new_state
+    else:
+        return make_love(top_g, K, PROP_LOVE)
+
+
+###################################################################################################
+for i in range(1000):
+    v = make_love(top_g, k, PROP_LOVE)
+
+    queue_push((-fitness(v, k), v), top_g, is_added, population)
+
+
+#################
+for c in range(100):
+    population_list_temp = take_out_take_back()
+    for i in range(100):
+        p1 = population_list_temp[random.randint(0,  MAX_SIZE-1)]
+        p2 = population_list_temp[random.randint(0, MAX_SIZE-1)]
+        v = make_love([p1, p2], k, PROP_LOVE)
+        queue_push((-fitness(v, k), v), [p1, p2], is_added, population)
+
+
+#################################
+temp_list = []
+
+i = 1
+while not population.empty():
+
+    item = population.get()
+
+    temp_list.append(item)
+    if i == MAX_SIZE:
+        print(item)  # In từng phần tử
+        print(i)
+    i += 1
+# Đưa lại các phần tử vào PriorityQueue
+best = item[1]
+for item in temp_list:
+    population.put(item)
+
+#################################
+# output
+print(k)
+for worker in range(k):
+    temp = []
+
+    for index in range(n):
+        if best[index] == worker:
+            temp.append(index + 1)
+
+    print()
+    print(len(temp) + 2)
+    print([0] + temp + [0])
+
+print("***********************************************")
+# Kết thúc đo thời gian
+end_time = time.time()
+
+# In thời gian chạy
+print(f"Thời gian chạy: {end_time - start_time:.2f} giây")
---
+++ tabusearch_optimize.py
@@ -0,0 +1,257 @@
+import random
+import numpy as np
+from collections import deque
+import sys
+import time
+
+
+start_time = time.time()
+
+file_path = "/mnt/c/Users/Admin/Desktop/code python/data.txt"
+
+# Đọc file và xử lý dữ liệu
+with open(file_path, "r") as f:
+    # Đọc dòng 1: chứa N và K
+    first_line = f.readline().strip()
+    n, k = map(int, first_line.split())
+
+    # Đọc dòng 2: chứa d(1), d(2), ..., d(N)
+    second_line = f.readline().strip()
+    d = [0] + list(map(int, second_line.split()))
+
+    # Đọc các dòng tiếp theo: ma trận t
+    distance_matrix = []
+    for _ in range(n+1):
+        row = list(map(int, f.readline().strip().split()))
+        distance_matrix.append(row)
+
+
+###############################################################
+
+
+def initialize_solution(n, k):
+    solution = [[] for _ in range(k)]
+
+    customers = list(range(1, n + 1))
+    for i in range(k):
+        solution[i] = optimize_route(customers[i::k])
+
+    return solution
+
+
+def initialize_solution_2(n, k):
+    solution = [[] for _ in range(k)]
+    customers = list(range(1, n + 1))
+    chunk_size = n // k  # Kích thước mỗi phần (chia nguyên)
+    remainder = n % k  # Số lượng khách hàng dư ra
+
+    start = 0
+    for i in range(k):
+        end = start + chunk_size
+
+        if remainder > 0:
+            end += 1
+            remainder -= 1
+        solution[i] = customers[start:end]
+        start = end
+
+    return solution
+
+
+def initialize_solution_3_random(n, k):
+    solution = [[] for _ in range(k)]
+    customers = list(range(1, n + 1))
+    remaining_customers = n - 3 * (k - 1)
+    for i in range(k):
+        solution[i].append(customers[i])
+        solution[i].append(customers[2*i])
+        solution[i].append(customers[3*i])
+
+    temp_i = 3 * (k - 1)
+    for v in range(remaining_customers):
+        random_index = random.randint(0, k - 1)
+        solution[random_index].append(customers[temp_i + v])
+
+    return solution
+
+
+def calculate_route_time(route):
+    route_temp = [0] + route + [0]
+    time_val = 0
+    for i in range(len(route_temp) - 1):
+        time_val += distance_matrix[route_temp[i]][route_temp[i + 1]]
+        time_val += d[route_temp[i + 1]]
+    return time_val
+
+
+def optimize_route(route):
+    # Tham lam
+    optimized_route = []
+    remaining_points = route[:]
+    current_point = 0
+
+    while remaining_points:
+        next_point = min(remaining_points,
+                         key=lambda x: distance_matrix[current_point][x])
+        optimized_route.append(next_point)
+        remaining_points.remove(next_point)
+        current_point = next_point
+
+    return optimized_route
+
+
+def calculate_total_time(solution):
+    times = [calculate_route_time(route)
+             for route in solution]
+    return times
+
+################################
+
+# chuẩn hóa dạng
+
+
+def canonical_form(solution):
+    # Sắp xếp các route theo một tiêu chí bất kỳ. Ở đây: dựa trên điểm đầu tiên (nếu route rỗng thì đặt giá trị -1)
+    sorted_routes = sorted(
+        solution, key=lambda route: route[0] if len(route) > 0 else -1)
+    # Chuyển mỗi route thành tuple (immutable) để có thể băm (hash)
+    canonical_solution = tuple(tuple(r) for r in sorted_routes)
+    return canonical_solution
+
+
+def tabu_search(solution, max_iter=60000):
+    # Thay vì chỉ dùng deque, ta dùng thêm set để kiểm tra nhanh
+    tabu_list = deque()
+    tabu_set = set()
+
+    best_solution = [route[:] for route in solution]
+    best_times = calculate_total_time(best_solution)
+    best_times_val = max(best_times)
+    length_tabu = 0
+    for iteration in range(max_iter):
+        prop = random.random()
+        times = calculate_total_time(solution)
+        sorted_times = sorted(
+            enumerate(times), key=lambda x: x[1], reverse=True)
+        #################################
+        if prop < 0.8:
+            max_idx, min_idx = sorted_times[0][0], sorted_times[-1][0]
+            temp_length = len(solution[max_idx])
+            if temp_length >= 1:
+                vertex = random.randint(0, temp_length - 1)
+                transfer_point = solution[max_idx][vertex]
+
+                solution[max_idx].remove(transfer_point)
+                solution[min_idx].insert(-1, transfer_point)
+
+                # Tối ưu lại lộ trình sau khi chuyển giao
+                solution[max_idx] = optimize_route(solution[max_idx])
+                solution[min_idx] = optimize_route(solution[min_idx])
+
+                # Chuẩn hóa lời giải hiện tại
+                canonical_solution = canonical_form(solution)
+                if canonical_solution in tabu_set:
+                    # Hoàn nguyên nếu muốn, ở đây có thể bỏ qua move
+                    # Hoàn nguyên
+                    solution[min_idx].remove(transfer_point)
+                    solution[max_idx].insert(vertex, transfer_point)
+                    continue
+
+            times = calculate_total_time(solution)
+            if max(times) < best_times_val:
+                best_solution = [route[:] for route in solution]
+                best_times = times
+                best_times_val = max(times)
+                # Thêm vào tabu
+                canonical_best = canonical_form(best_solution)
+                tabu_list.append(canonical_best)
+                tabu_set.add(canonical_best)
+                length_tabu += 1
+            if length_tabu >= 10000:
+                oldest = tabu_list.popleft()
+                tabu_set.remove(oldest)
+                length_tabu -= 1
+
+        # elif prop < 0.1:
+        #     max_idx, min_idx = sorted_times[random.randint(
+        #         0, int(k/2))][0], sorted_times[random.randint(int(k/2) + 1, k-1)][0]
+
+        #     temp_length = len(solution[max_idx])
+        #     if temp_length >= 1:
+        #         vertex = random.randint(0, temp_length - 1)
+        #         transfer_point = solution[max_idx][vertex]
+
+        #         solution[max_idx].remove(transfer_point)
+        #         solution[min_idx].insert(-1, transfer_point)
+
+        #         solution[max_idx] = optimize_route(solution[max_idx])
+        #         solution[min_idx] = optimize_route(solution[min_idx])
+
+        #         canonical_solution = canonical_form(solution)
+        #         if canonical_solution in tabu_set:
+        #             # Hoàn nguyên
+        #             solution[min_idx].remove(transfer_point)
+        #             solution[max_idx].insert(vertex, transfer_point)
+        #             continue
+
+        #     times = calculate_total_time(solution)
+        #     if max(times) < best_times_val:
+        #         best_solution = [route[:] for route in solution]
+        #         best_times = times
+        #         best_times_val = max(times)
+        #         canonical_best = canonical_form(best_solution)
+        #         tabu_list.append(canonical_best)
+        #         tabu_set.add(canonical_best)
+        #         length_tabu += 1
+        #     if length_tabu >= 10000:
+        #         oldest = tabu_list.popleft()
+        #         tabu_set.remove(oldest)
+        #         length_tabu -= 1
+
+        else:
+            max_idx, min_idx = sorted_times[random.randint(
+                int(k/3),  int(k/2))][0], sorted_times[random.randint(
+                    int(k/1.4), k-1)][0]
+
+            temp_length = len(solution[max_idx])
+            if temp_length >= 1:
+                vertex = random.randint(0, temp_length - 1)
+                transfer_point = solution[max_idx][vertex]
+
+                # just swap
+                old_val = solution[min_idx][-1]
+                solution[max_idx][vertex], solution[min_idx][-1] = old_val, transfer_point
+
+                solution[max_idx] = optimize_route(solution[max_idx])
+                solution[min_idx] = optimize_route(solution[min_idx])
+
+                canonical_solution = canonical_form(solution)
+                if canonical_solution in tabu_set:
+                    # Hoàn nguyên
+                    solution[max_idx][vertex], solution[min_idx][-1] = transfer_point, old_val
+                    continue
+
+            times = calculate_total_time(solution)
+            if max(times) < best_times_val:
+                best_solution = [route[:] for route in solution]
+                best_times = times
+                best_times_val = max(times)
+                canonical_best = canonical_form(best_solution)
+                tabu_list.append(canonical_best)
+                tabu_set.add(canonical_best)
+                length_tabu += 1
+            if length_tabu >= 10000:
+                oldest = tabu_list.popleft()
+                tabu_set.remove(oldest)
+                length_tabu -= 1
+
+    return best_solution, best_times
+
+
+initial_solution = initialize_solution_3_random(n, k)
+optimized_solution, optimized_times = tabu_search(initial_solution)
+print("Total Times:", max(optimized_times))
+
+print("***********************************************")
+end_time = time.time()
+print(f"Thời gian chạy: {end_time - start_time:.2f} giây")
